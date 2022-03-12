20ee1b85b316dda2c4d5b48522622d61cb4e1212
Alissa <alissa@Zipcoders-MBP-3.hsd1.de.comcast.net>
2022-03-12 16:40:30 -0500

    Starvation to immigrants

---
+++ src/main/java/hammurabi/Hammurabi.java
@@ -0,0 +1,77 @@
+package hammurabi;               // package declaration
+
+import java.util.Random;         // imports go here
+import java.util.Scanner;
+
+public class Hammurabi {
+
+    Random rand = new Random();
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        new Hammurabi().playGame();
+    }
+
+    void playGame() {
+
+        int population = 100;
+        int bushelsGrain = 2800;
+        int acresLand = 1000;
+        int landValueBushelsPerAcre = 19;
+        int year = 0;
+        // "statements go after declarations" --- may the ones that reference other methods?
+
+
+    }
+
+            //Alissa's Methods int Starv to Immmigrants done so far
+    public int starvationDeaths(int population, int bushelsFedToPeople) {
+
+        //if more ppl are over fed grain =0
+        int fedPpl = bushelsFedToPeople / 20;     // Each person needs 20 grains of bushels to surv
+        int starvDeath = 0;
+
+        if (fedPpl < population) {
+            starvDeath = population - fedPpl;
+        }
+        return starvDeath;
+
+    }
+
+    public boolean uprising(int population, int howManyPeopleStarved) {
+        //return true if 45% of ppl starve
+        double percentage = (double) howManyPeopleStarved / population; //double div by int =double
+        boolean didMoreThan45PercDie = percentage < 0.45;
+
+
+        return didMoreThan45PercDie;
+    }
+
+    public int immigrants(int population, int acresOwned, int grainInStorage) {
+
+        int immigtrantWave = (20 * acresOwned + grainInStorage) / (100 * population) + 1; // this call is already an int no need to cast
+
+        return immigtrantWave;
+    }
+
+    public int harvest(int acres, int bushelsUsedAsSeed) {
+        //get random int from 1-6
+
+        return 0; // place holder
+    }
+
+    public int grainEatenByRats(int bushels) {
+        return 0; // place holder
+    }
+
+    public int newCostOfLand() {
+        return 0; // place holder
+    }
+
+}
+
+
+
+
