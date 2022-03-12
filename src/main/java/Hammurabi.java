548fd6c3391873c70182293b71f00a9053661b88
Meredith <meredith@Zipcoders-MacBook-Pro-2.local>
2022-03-11 21:03:48 -0500

    added skeleton

--- src/main/java/Hammurabi.java
+++
@@ -1,7 +0,0 @@
-
-public class Hammurabi {
-
-    public void print() {
-        System.out.print("Hello World");
-    }
-}
---
+++ src/main/java/hammurabi/Hammurabi.java
@@ -0,0 +1,22 @@
+package hammurabi;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hammurabi {
+    Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        new Hammurabi().playGame();
+    }
+
+    void playGame() {
+        // grain
+        // population
+        // etc.
+        // "statements go after declarations" --- may the ones that reference other methods?
+    }
+
+    // other methods go here
+}
