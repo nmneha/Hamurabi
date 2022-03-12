e921955d9b83b385aae30df3781d4939272b79b9
Meredith <meredith@Zipcoders-MacBook-Pro-2.local>
2022-03-12 00:54:40 -0500

    created askHowManyACresToSell method

--- src/main/java/hammurabi/Hammurabi.java
+++ src/main/java/hammurabi/Hammurabi.java
@@ -27,14 +27,24 @@ public class Hammurabi {
         int landValueBushelsPerAcre = 19;
         int year = 0;
         int numberOfAcresToBuy = 0;
-        int bushelsSpent = 0;
+        int numberOfAcresToSell = 0;
         // "statements go after declarations" --- maybe the ones that reference other methods?
-            numberOfAcresToBuy = askHowManyAcresToBuy(landValueBushelsPerAcre, bushelsGrain, acresLand);
-            acresLand += numberOfAcresToBuy;
-            bushelsSpent = landValueBushelsPerAcre * numberOfAcresToBuy;
-            bushelsGrain -= bushelsSpent;
-            numberOfAcresToBuy = 0;
-            bushelsSpent = 0;
+
+        // numberOfAcresToBuy
+        numberOfAcresToBuy = askHowManyAcresToBuy(landValueBushelsPerAcre, bushelsGrain, acresLand);
+        acresLand += numberOfAcresToBuy;
+        bushelsGrain -= landValueBushelsPerAcre * numberOfAcresToBuy;
+
+        // numberOfAcresToSell
+        if(numberOfAcresToBuy == 0) {
+            numberOfAcresToSell = askHowManyAcresToSell(acresLand);
+            acresLand -= numberOfAcresToSell;
+            bushelsGrain += landValueBushelsPerAcre * numberOfAcresToSell;
+        }
+
+        // reset temporary variables
+        numberOfAcresToBuy = 0;
+        numberOfAcresToSell = 0;
 
         System.out.println(acresLand); // just for testing
         System.out.println(bushelsGrain); // just for testing
@@ -42,8 +52,8 @@ public class Hammurabi {
 
     public int askHowManyAcresToBuy(int price, int bushelsGrain, int acresLand) {
         System.out.println("How many acres would you like to buy?");
-        Scanner scanner1 = new Scanner(System.in);
-        int numberOfAcresToBuy = Integer.parseInt(scanner1.nextLine());
+        // Scanner scanner1 = new Scanner(System.in);
+        int numberOfAcresToBuy = Integer.parseInt(scanner.nextLine());
         if(numberOfAcresToBuy >= 0 && price * numberOfAcresToBuy <= bushelsGrain) {
             System.out.println("You bought " + numberOfAcresToBuy + " acres.");
         } else if(numberOfAcresToBuy < 0 && price * numberOfAcresToBuy <= bushelsGrain) {
@@ -53,8 +63,25 @@ public class Hammurabi {
             System.out.println("Invalid Entry: Price Exceeds Bushels In Storage");
             askHowManyAcresToBuy(price, bushelsGrain, acresLand);
         }
-        scanner1.close();
+        // scanner1.close();
         return numberOfAcresToBuy;
+    }
+
+    public int askHowManyAcresToSell(int acresLand) {
+        System.out.println("How many acres would you like to sell?");
+        // Scanner scanner2 = new Scanner(System.in);
+        int numberOfAcresToSell = Integer.parseInt(scanner.nextLine());
+        if(numberOfAcresToSell >= 0 && numberOfAcresToSell <= acresLand) {
+            System.out.println("You sold " + numberOfAcresToSell + " acres.");
+        } else if(numberOfAcresToSell < 0) {
+            System.out.println("Invalid Entry: Negative Number");
+            askHowManyAcresToSell(acresLand);
+        } else if (numberOfAcresToSell > acresLand) {
+            System.out.println("Invalid Entry: Input Exceeds Acres Owned");
+            askHowManyAcresToSell(acresLand);
+        }
+        // scanner2.close();
+        return numberOfAcresToSell;
     }
 
     // other methods go here
