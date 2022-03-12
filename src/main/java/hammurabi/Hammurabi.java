d41ef3525d381bbe0ab0d591de03cc3848e4014d
Meredith <meredith@Zipcoders-MacBook-Pro-2.local>
2022-03-12 00:30:14 -0500

    revised askHowManyAcresToBuy method

--- src/main/java/hammurabi/Hammurabi.java
+++ src/main/java/hammurabi/Hammurabi.java
@@ -9,15 +9,8 @@ public class Hammurabi {
     Random random = new Random();
     Scanner scanner = new Scanner(System.in);
 
-    int population = 100;
-    int bushelsGrain = 2800;
-    int acresLand = 1000;
-    int landValueBushelsPerAcre = 19;
-    int year = 0;
-    // "statements go after declarations" --- may the ones that reference other methods?
-
     public static void main(String[] args) {
-        new Hammurabi().playGame();
+        new Hammurabi().playGame(); // new Hammurabi object that calls the playGame method
     }
 
     // public static String getStringInput(String prompt) {
@@ -28,29 +21,38 @@ public class Hammurabi {
     // }
 
     void playGame() {
+        int population = 100;
+        int bushelsGrain = 2800;
+        int acresLand = 1000;
+        int landValueBushelsPerAcre = 19;
+        int year = 0;
+        int numberOfAcresToBuy = 0;
+        int bushelsSpent = 0;
+        // "statements go after declarations" --- maybe the ones that reference other methods?
+            numberOfAcresToBuy = askHowManyAcresToBuy(landValueBushelsPerAcre, bushelsGrain, acresLand);
+            acresLand += numberOfAcresToBuy;
+            bushelsSpent = landValueBushelsPerAcre * numberOfAcresToBuy;
+            bushelsGrain -= bushelsSpent;
+            numberOfAcresToBuy = 0;
+            bushelsSpent = 0;
 
+        System.out.println(acresLand); // just for testing
+        System.out.println(bushelsGrain); // just for testing
     }
 
-    public int askHowManyAcresToBuy(int price, int bushels) {
+    public int askHowManyAcresToBuy(int price, int bushelsGrain, int acresLand) {
         System.out.println("How many acres would you like to buy?");
         Scanner scanner1 = new Scanner(System.in);
         int numberOfAcresToBuy = Integer.parseInt(scanner1.nextLine());
-        acresLand += numberOfAcresToBuy;
-        if(numberOfAcresToBuy >= 0) {
-
-        } else {
+        if(numberOfAcresToBuy >= 0 && price * numberOfAcresToBuy <= bushelsGrain) {
+            System.out.println("You bought " + numberOfAcresToBuy + " acres.");
+        } else if(numberOfAcresToBuy < 0 && price * numberOfAcresToBuy <= bushelsGrain) {
             System.out.println("Invalid Entry: Negative Number");
-            askHowManyAcresToBuy(price, bushels);
-        }
-        int bushelsSpent = price * numberOfAcresToBuy;
-        if(bushelsSpent <= bushelsGrain) {
-
-        } else {
+            askHowManyAcresToBuy(price, bushelsGrain, acresLand);
+        } else if (numberOfAcresToBuy >= 0 && price * numberOfAcresToBuy > bushelsGrain) {
             System.out.println("Invalid Entry: Price Exceeds Bushels In Storage");
-            askHowManyAcresToBuy(price, bushels);
+            askHowManyAcresToBuy(price, bushelsGrain, acresLand);
         }
-        bushelsGrain -= bushelsSpent;
-        System.out.println("You bought " + numberOfAcresToBuy + "acres.");
         scanner1.close();
         return numberOfAcresToBuy;
     }
